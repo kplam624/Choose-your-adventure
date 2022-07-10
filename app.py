f56c3cd32bf71781e2c8e2391d97bab12f5875be
kplam624 <kplam624@gmail.com>
2022-07-10 19:57:18 -0400

    Added routes for user creation and route for login page.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 import datetime
 import os
 import socket
@@ -11,15 +11,25 @@ def home():
     print('Responding to the home route: ',datetime.datetime.now())
     return render_template('index.html')
 
+# route for the adventure page
 app.route('/adventure')
 def adventure():
     print('Responding to the request for the adventure route.')
     return render_template('adventure.html')
-    
-# route for the user
+
+# route for the login page
+app.route('/login')
+def login():
+    print('Responding to the login page.')
+    return render_template('')
+
+# route to create a user.
 app.route('/user', methods=['POST','GET'])
-def createuser():
+def createuser(user, password):
     print('Responding to the user page.')
+    # Should check if this user has been made or not.
+
+    # Database here.
 
 
 if __name__ == "__main__":
