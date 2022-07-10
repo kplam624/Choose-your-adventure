23bbf3158fc3f97b0f8d7b0f5557a54b785541e8
kplam624 <kplam624@gmail.com>
2022-07-10 19:45:46 -0400

    Adding more routes to the app.py file. Looking to build out login connection.

--- app.py
+++ app.py
@@ -5,10 +5,21 @@ import socket
 
 app = Flask(__name__)
 
+# route for the home page
 app.route('/')
 def home():
     print('Responding to the home route: ',datetime.datetime.now())
+    return render_template('index.html')
 
+app.route('/adventure')
+def adventure():
+    print('Responding to the request for the adventure route.')
+    return render_template('adventure.html')
+    
+# route for the user
+app.route('/user', methods=['POST','GET'])
+def createuser():
+    print('Responding to the user page.')
 
 
 if __name__ == "__main__":
